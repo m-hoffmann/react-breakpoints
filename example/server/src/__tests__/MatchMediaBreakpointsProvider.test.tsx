6ff5d4130427a4c7890fdb3258fc1cb0a8a127ff
Markus Hoffmann <markus.hoffmann@outlook.com>
2022-02-03 00:27:50 +0100

    Install latest on server example and add test for new component

---
+++ example/server/src/__tests__/MatchMediaBreakpointsProvider.test.tsx
@@ -0,0 +1,146 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-children-prop */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+
+import {
+  MediaMatchBreakpointsProvider,
+  useBreakpoints,
+} from 'react-hook-breakpoints';
+
+describe('MediaMatchBreakpointsProvider on the server', function () {
+  type Breakpoint = 'desktop' | 'tablet' | 'mobile';
+
+  function CurrentBreakpoint() {
+    const props = useBreakpoints<Breakpoint>();
+
+    return <>{props.currentBreakpoint}</>;
+  }
+
+  function ScreenWidth() {
+    const props = useBreakpoints<Breakpoint>();
+
+    return <>{props.screenWidth}</>;
+  }
+
+  function Breakpoints() {
+    const props = useBreakpoints<Breakpoint>();
+    return <>{JSON.stringify(props.breakpoints)}</>;
+  }
+
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {
+      /* linter */
+    });
+  });
+
+  it('explodes if there are no breakpoints', function () {
+    expect(() =>
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={null as any}
+          children={<CurrentBreakpoint />}
+        />,
+      ),
+    ).toThrow();
+    expect(() =>
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={123 as any}
+          children={<CurrentBreakpoint />}
+        />,
+      ),
+    ).toThrow();
+
+    expect(() =>
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={{}}
+          children={<CurrentBreakpoint />}
+        />,
+      ),
+    ).toThrow();
+  });
+
+  it.skip('works without window and just the breakpoints, detects mobile', function () {
+    const breakpoints = { mobile: 320, tablet: 768, desktop: 1200 };
+
+    expect(
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={breakpoints}
+          children={<CurrentBreakpoint />}
+        />,
+      ),
+    ).toBe('mobile');
+  });
+
+  it.skip('works without window and uses guessedBreakpoint desktop', function () {
+    const breakpoints = { mobile: 320, tablet: 768, desktop: 1200 };
+
+    expect(
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={breakpoints}
+          children={<CurrentBreakpoint />}
+        />,
+      ),
+    ).toBe('desktop');
+  });
+
+  it.skip('works without window and uses guessedBreakpoint tablet', function () {
+    const breakpoints = { mobile: 320, tablet: 768, desktop: 1200 };
+
+    expect(
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={breakpoints}
+          children={<CurrentBreakpoint />}
+        />,
+      ),
+    ).toBe('tablet');
+  });
+
+  it.skip('works without window and uses guessedBreakpoint mobile', function () {
+    const breakpoints = { mobile: 320, tablet: 768, desktop: 1200 };
+
+    expect(
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={breakpoints}
+          children={<CurrentBreakpoint />}
+        />,
+      ),
+    ).toBe('mobile');
+  });
+
+  it('Detects screenWidth 0', function () {
+    const breakpoints = { mobile: 320, tablet: 768, desktop: 1200 };
+
+    expect(
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={breakpoints}
+          children={<ScreenWidth />}
+        />,
+      ),
+    ).toBe('0');
+  });
+
+  it('Renders breakpoints', function () {
+    const breakpoints = { mobile: 320, tablet: 768, desktop: 1200 };
+
+    expect(
+      renderToStaticMarkup(
+        <MediaMatchBreakpointsProvider
+          breakpoints={breakpoints}
+          children={<Breakpoints />}
+        />,
+      ),
+    ).toBe(
+      '{&quot;mobile&quot;:320,&quot;tablet&quot;:768,&quot;desktop&quot;:1200}',
+    );
+  });
+});
